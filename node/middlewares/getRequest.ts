d1f74773cf8a2f0416032e45f3ee9c3df8dbcc6e
Elkin Urango <kin3xc@hotmail.com>
2023-03-14 09:34:48 -0500

    refactor: removed innesesary code

--- node/middlewares/getRequest.ts
+++
@@ -1,9 +0,0 @@
-import { returnRequestService } from '../services/returnRequestService'
-
-export async function getRequest(ctx: Context) {
-  const { requestId } = ctx.vtex.route.params as { requestId: string }
-
-  ctx.set('Cache-Control', 'no-cache')
-
-  ctx.body = await returnRequestService(ctx, requestId)
-}
