338b2cd30dba533c7a87fcff1f26f6f80eead3a3
Nmanrique <nmanriqueav@gmail.com>
2024-05-21 11:09:59 -0500

    Add changes on orderToReturnSummary

--- node/resolvers/orderToReturnSummary.ts
+++ node/resolvers/orderToReturnSummary.ts
@@ -1,10 +1,9 @@
 import { ResolverError, UserInputError } from '@vtex/api'
-import { createOrdersToReturnSummary } from '../utils/createOrdersToReturnSummary'
 import { isUserAllowed } from '../utils/isUserAllowed'
 import { canOrderBeReturned } from '../utils/canOrderBeReturned'
-import { getCustomerEmail } from '../utils/getCostumerEmail'
 import type { OrderToReturnSummary } from '../../typings/OrdertoReturn'
 import { DEFAULT_SETTINGS } from '../clients/settings'
+import orderToReturnSummaryService from '../services/summary/orderToReturnSummaryService'
 
 export const orderToReturnSummary = async (
   _: unknown,
@@ -14,17 +13,7 @@ export const orderToReturnSummary = async (
   const { orderId, storeUserEmail } = args
   const {
     state: { userProfile, appkey },
-    clients: {
-      returnSettings,
-      oms,
-      order: orderRequestClient,
-      catalog,
-      catalogGQL,
-      account: accountClient,
-      settingsAccount,
-      profile,
-    },
-    vtex: { logger, adminUserAuthToken },
+    clients: { returnSettings, oms, account: accountClient, settingsAccount },
   } = ctx
 
   const accountInfo = await accountClient.getInfo()
@@ -61,91 +50,12 @@ export const orderToReturnSummary = async (
     orderStatus: settings.orderStatus,
   })
 
-  if (userProfile?.role === 'admin') {
-    try {
-      const profileUnmask = await profile.getProfileUnmask(
-        clientProfileData?.userProfileId,
-        adminUserAuthToken,
-        accountInfo?.parentAccountName || appConfig.parentAccountName
-      )
-
-      if (profileUnmask?.[0]?.document?.email) {
-        const currentProfile = profileUnmask?.[0]?.document
-        order.clientProfileData = {
-          ...order.clientProfileData,
-          email: currentProfile.email,
-          firstName: currentProfile?.firstName,
-          lastName: currentProfile?.lastName,
-          phone: currentProfile?.homePhone,
-        }
-      } else {
-        const response = await profile.searchEmailByUserId(
-          clientProfileData?.userProfileId,
-          adminUserAuthToken,
-          accountInfo?.parentAccountName || appConfig.parentAccountName
-        )
-
-        if (response.length > 0) {
-          const currentProfile = response?.[0]
-          order.clientProfileData = {
-            ...order.clientProfileData,
-            email: currentProfile?.email,
-            firstName: currentProfile?.email?.firstName,
-            lastName: currentProfile?.email?.lastName,
-            phone: currentProfile?.email?.phone,
-          }
-        }
-      }
-    } catch (error) {}
-
-    try {
-      const addressUnmask = await profile.getAddressUnmask(
-        clientProfileData?.userProfileId,
-        adminUserAuthToken,
-        accountInfo?.parentAccountName || appConfig.parentAccountName
-      )
-
-      if (addressUnmask?.[0]?.document) {
-        const address = addressUnmask?.[0]?.document
-        order.shippingData.address = {
-          ...order.shippingData.address,
-          receiverName: address.receiverName,
-          city: address.locality,
-          postalCode: address.postalCode,
-          street: address.route,
-          number: address.streetNumber,
-        }
-      }
-    } catch (error) {}
-  }
-
-  const customerEmail = getCustomerEmail(
-    clientProfileData,
-    {
-      userProfile,
-      appkey,
-      inputEmail: storeUserEmail || clientProfileData?.email,
-    },
-    {
-      logger,
-    }
-  )
-
-  return createOrdersToReturnSummary(
+  return orderToReturnSummaryService(
+    ctx,
     order,
-    customerEmail,
-    accountInfo?.parentAccountName
-      ? { ...accountInfo, isSellerPortal: false }
-      : {
-          ...appConfig,
-          isSellerPortal: true,
-          accountName: accountInfo.accountName,
-        },
-    {
-      excludedCategories: settings.excludedCategories,
-      orderRequestClient,
-      catalog,
-      catalogGQL,
-    }
+    accountInfo,
+    settings,
+    appConfig,
+    storeUserEmail
   )
 }
